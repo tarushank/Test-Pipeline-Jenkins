6c5108c7e7a2da3e63e1f9a370a4a8eceb7ca95e
tarushank <tarushank@gmail.com>
2021-04-22 17:33:03 -0400

    initial commit

---
+++ src/main/java/com/testpipeline/demo/to/test/pipeline/with/Jenkins/DemoToTestPipelineWithJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.testpipeline.demo.to.test.pipeline.with.Jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoToTestPipelineWithJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoToTestPipelineWithJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testpipeline/demo/to/test/pipeline/with/Jenkins/DemoToTestPipelineWithJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testpipeline.demo.to.test.pipeline.with.Jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoToTestPipelineWithJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
